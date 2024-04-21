ecfa76ffb45037037c0a83b242ea4136de2f28be
sallyw <sallyw@sallywnoMacBook-Air.local>
2024-04-21 11:47:35 +0900

    added

--- helper_functions/define_linear_models.py
+++ helper_functions/define_linear_models.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn import linear_model
+from sklearn import linear_model, svm, neighbors, tree
 from sklearn.metrics import mean_squared_error
 
 
@@ -24,7 +24,11 @@ def define_linear_model(X_train, y_train):
         ('Ridge', linear_model.Ridge(alpha=0.5)),
         ('Lasso', linear_model.Lasso(alpha=0.5)),
         ('ElasticNet', linear_model.ElasticNet(l1_ratio=0.5)),
-        ('SGDRegressor', linear_model.SGDRegressor())
+        ('SGDRegressor', linear_model.SGDRegressor()),
+        ('SVR', svm.SVR(kernel="linear")),
+        ('KNeighbors', neighbors.KNeighborsRegressor(n_neighbors=5)),
+        ('DTRegressor', tree.DecisionTreeRegressor())
+        ('BayesianRidge', linear_model.BayesianRidge())
         # Add more linear models if needed
     ]
 
